e5d123b41c12945d660d55105d8eedb875e7389f
felipemarinzeck <luiz.marinzeck@valecard.com.br>
2022-05-22 19:09:53 -0300

    - Correção na inserção inicial dos registros para que seja gerado o ID automaticamente sem precisar mudar a estrutura do arquivo 'movielist.csv';

---
+++ src/main/java/com/texoit/movies/entities/Movie.java
@@ -0,0 +1,26 @@
+//package com.texoit.movies.entities;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class Movie {
+//
+//  @Id
+//  private Long id;
+//
+//  private Integer year;
+//
+//  private String title;
+//
+//  private String studios;
+//
+//  private String producers;
+//
+//  private Boolean winner;
+//
+//}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-CREATE TABLE MOVIE AS SELECT * FROM CSVREAD('classpath:static/movielist.csv',null,'fieldSeparator=;');
+CREATE TABLE MOVIE("ID" bigint auto_increment, "YEAR" bigint, "TITLE" varchar(255), "STUDIOS" varchar(255), "PRODUCERS" varchar(255), "WINNER" boolean);
+
+insert into movie("YEAR", "TITLE", "STUDIOS", "PRODUCERS", "WINNER")
+select "YEAR", "TITLE", "STUDIOS", "PRODUCERS", "WINNER" FROM CSVREAD('classpath:static/movielist.csv',null,'fieldSeparator=;')
